f1ee5a96609ee9d41872c757474403df8bf1e365
Jan De Laet <jan.delaet@dexmach.com>
2023-12-05 02:24:06 +0100

    test

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
-from flask import request, render_template, make_response
-
-from server.webapp import flaskapp, cursor
-from server.models import Book
+import sqlite3
 
 
-@flaskapp.route("/")
-def index():
-    name = request.args.get("name")
-    author = request.args.get("author")
-    read = bool(request.args.get("read"))
+# @flaskapp.route("/")
+# def index():
+#     name = request.args.get("name")
+#     author = request.args.get("author")
+#     read = bool(request.args.get("read"))
 
-    if name:
-        cursor.execute(
-            "SELECT * FROM books WHERE name LIKE :name", {"name": f"%{name}%"}
-        )
-        books = [Book(*row) for row in cursor]
+#     if name:
+#         cursor.execute(
+#             "SELECT * FROM books WHERE name LIKE :name", {"name": f"%{name}%"}
+#         )
+#         books = [Book(*row) for row in cursor]
 
-    elif author:
-        cursor.execute(
-            "SELECT * FROM books WHERE author LIKE :author", {"author": f"%{author}%"}
-        )
-        books = [Book(*row) for row in cursor]
+#     elif author:
+#         cursor.execute(
+#             "SELECT * FROM books WHERE author LIKE :author", {"author": f"%{author}%"}
+#         )
+#         books = [Book(*row) for row in cursor]
 
-    else:
-        cursor.execute("SELECT name, author, read FROM books")
-        books = [Book(*row) for row in cursor]
+#     else:
+#         cursor.execute("SELECT name, author, read FROM books")
+#         books = [Book(*row) for row in cursor]
 
-    return render_template("books.html", books=books)
+#     return render_template("books.html", books=books)
+
+
+database_uri = ":memory"
+
+database = sqlite3.connect(database_uri, check_same_thread=False)
+cursor = database.cursor()
+
+cursor.execute()
+
+name = "jan"
+
+if name:
+    cursor.execute("SELECT * FROM books WHERE name LIKE :name", {"name": f"%{name}%"})
+
+else:
+    cursor.execute("SELECT name, author, read FROM books")
